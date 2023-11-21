27e581b6c2d239b4ec69018a2c493496cbb4a8a9
Somya Bhatt <somyacu1@gmail.com>
2023-11-21 12:03:57 +0530

    api req

---
+++ src/requests/index.js
@@ -0,0 +1,53 @@
+import {
+    updateQuery,
+    searchQuery,
+} from '../query/index.js'
+
+export const searchAnime = async(input) => {
+    try {
+        const variables  = {
+            search: input,
+            type: "ANIME",
+        }
+
+        const response = await fetch("https://graphql.anilist.co", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ query: searchQuery, variables }),
+        });
+
+      const data = await response.json();
+      return data.data.Page.media;
+
+    } catch (error) {
+      console.log("Error in searching data",error);  
+    }
+}
+
+export const updateAnime = async(variables) => {
+    try {
+        
+        // get token from local storage
+        const accessToken = localStorage.getItem('accessToken');
+
+        const response = await fetch('https://graphql.anilist.co', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query: updateQuery, variables }),
+        })
+
+        const data = await response.json();
+        console.log("data from updateAnime",data);
+        return data?.data?.SaveMediaListEntry;
+
+    } catch (error) {
+        console.log("Error in updating data",error);
+    }
+}
+
