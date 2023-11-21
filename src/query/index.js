4bd54855330d40e2fafb6121813f3a4aa96a7052
Somya Bhatt <somyacu1@gmail.com>
2023-11-21 12:03:29 +0530

    queries for searching/updating

---
+++ src/query/index.js
@@ -0,0 +1,74 @@
+// queries.js
+
+const searchQuery = `
+  query ($search: String, $type: MediaType, $id: Int) {
+    Page {
+      media (search: $search, type: $type, id: $id) {
+        id
+        title {
+          romaji
+          english
+          native
+        }
+        episodes
+        genres
+        season
+        seasonYear
+        genres
+        status
+        format
+        duration
+        startDate {
+          year
+          month
+          day
+        }
+        endDate {
+          year
+          month
+          day
+        }
+        coverImage {
+          medium
+        }
+        bannerImage
+      }
+    }
+  }
+`;
+
+const updateQuery = `
+  mutation ($mediaId: Int, $status: MediaListStatus, $progress: Int, $score: Float, $repeat: Int, $startedAt: FuzzyDateInput, $completedAt: FuzzyDateInput) {
+    SaveMediaListEntry(mediaId: $mediaId, status: $status, progress: $progress, score: $score, repeat: $repeat, startedAt: $startedAt, completedAt: $completedAt) {
+      id
+      status
+      score
+      progress
+      repeat
+      startedAt {
+        year
+        month
+        day
+      }
+      completedAt {
+        year
+        month
+        day
+      }
+      media{
+          title{
+            english
+            romaji
+            native
+          }
+          coverImage{
+            medium
+          }
+          bannerImage
+          siteUrl
+        }
+    }
+  }
+`;
+
+export { updateQuery, searchQuery}
