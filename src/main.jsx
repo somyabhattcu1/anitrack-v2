88b542d4b550d46fdfd941daa9fe7f3ed49ea422
Somya Bhatt <somyacu1@gmail.com>
2023-11-21 12:04:34 +0530

    init

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { AppProvider } from './context/AppContext.jsx'
+import {BrowserRouter} from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <AppProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AppProvider>
+  </BrowserRouter>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Login from "./Login";
+import Search from "./Search";
+import Home from "./Home";
+import Update from "./Update";
+export { Login, Search, Home, Update };
