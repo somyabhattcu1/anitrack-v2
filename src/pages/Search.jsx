3f59796542759ac1f722e8ee9b2a70856951c233
Somya Bhatt <somyacu1@gmail.com>
2023-11-26 18:48:29 +0530

    added loading state

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,19 +3,24 @@ import AnimeDetails from "../components/AnimeDetails";
 import {searchAnime} from "../requests";
 import { useAppContext } from "../context/AppContext";
 import Update from "./Update";
+import {Triangle} from 'react-loader-spinner'
 
 
 const Search = () => {
   const [input, setInput] = useState("");
   const [data, setData] = useState([]);
 
+  const [loading,setLoading] = useState(false);
+
   const {titleClick} = useAppContext()
 
 
   const fetchData = async() => {
     try {
+      setLoading(true);
       let data = await searchAnime(input);
       setData(data);
+      setLoading(false);
     } catch (error) {
       console.error("Error during fetching data",error);
     }
@@ -41,11 +46,25 @@ const Search = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <div className="max-w-[400px] overflow-y-scroll scrollbar"> 
-        {data?.map((anime) => {
-          return (<AnimeDetails anime={anime} key={anime.id} />)
-        })}
-      </div>
+      {
+        loading? (
+          <div className="flex h-[500px] justify-center mt-2 items-center">
+            <Triangle 
+              height={50}
+              width={50}
+              color={'white'}
+            />
+          </div>
+          ) : 
+        (
+          <div className="max-w-[400px] overflow-y-scroll scrollbar"> 
+          {data?.map((anime) => {
+            return (<AnimeDetails anime={anime} key={anime.id} />)
+          })}
+        </div>
+        )
+      }
+
     </div>
   ) : (
     <Update />
--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,137 +1,156 @@
-import React from 'react'
-import { useEffect , useState} from 'react';
-import { updateAnime } from '../requests';
-import {formatDate} from '../utils/index';
-import { useAppContext } from '../context/AppContext';
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { updateAnime } from "../requests";
+import { formatDate } from "../utils/index";
+import { useAppContext } from "../context/AppContext";
+import {Triangle} from 'react-loader-spinner'
 
 const Update = () => {
+  const { animeId } = useAppContext();
+  const [animeData, setAnimeData] = useState([]);
 
-    const {animeId} = useAppContext();
-    const [animeData,setAnimeData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    // will be updated later
-    // const [loading,setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    status: "",
+    score: 0,
+    episodes: 0,
+    repeat: 0,
+    startDate: 0,
+    finishDate: undefined,
+  });
 
-    const [formData,setFormData] = useState({
-        status: '',
-        score: 0,
-        episodes: 0,
-        repeat: 0,
-        startDate: 0,
-        finishDate: undefined
-    })
-
-    const fetchData = async() => {
-        // setLoading(true);
-        try {
-            const variables = {
-                mediaId: animeId,
-            }
-            let data = await updateAnime(variables);
-            setAnimeData(data);
-            // setLoading(false);
-            setFormData
-            (prev => ({
-                ...prev,
-                status: data?.status,
-                score: data?.score,
-                episodes: data?.progress,
-                repeat: data?.repeat,
-                startDate: formatDate(data?.startedAt),
-                finishDate: (data.completedAt.year) && formatDate(data?.completedAt)
-            })
-            )
-        } catch (error) {
-            console.error("Error during fetching data",error);
-        }
+  const fetchData = async () => {
+    try {
+      const variables = {
+        mediaId: animeId,
+      };
+      setLoading(true);
+      let data = await updateAnime(variables);
+      setAnimeData(data);
+      setFormData((prev) => ({
+        ...prev,
+        status: data?.status,
+        score: data?.score,
+        episodes: data?.progress,
+        repeat: data?.repeat,
+        startDate: formatDate(data?.startedAt),
+        finishDate: data.completedAt.year && formatDate(data?.completedAt),
+      }));
+      setLoading(false);
+    } catch (error) {
+      console.error("Error during fetching data", error);
     }
+  };
 
-    const submitHandler = async(e) => {
-        e.preventDefault();
-        const variables = {
-            mediaId: animeId,
-            status: formData.status,
-            score: formData.score,
-            progress: formData.episodes,
-            repeat: formData.repeat,
-        }
-        updateAnime(variables);
-    }
-    
-    useEffect(() => {
-        fetchData();
-    },[]);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const variables = {
+      mediaId: animeId,
+      status: formData.status,
+      score: formData.score,
+      progress: formData.episodes,
+      repeat: formData.repeat,
+    };
+    updateAnime(variables);
+  };
 
-    return (
-        <form onSubmit={submitHandler} className='w-[350px] font-display max-h-[600px] p-3 flex flex-col bg-darkGrey'>
-            <h1 className='text-white'>{(animeData?.media?.title?.english)}</h1>
-            <img src={animeData?.media?.bannerImage} className='mt-2 rounded-sm' alt="" />
-            <label className='update-label'>
-                Status
-                <select 
-                    className='label-element'
-                    value={formData.status}
-                    onChange={(e) => setFormData(prev => ({...prev, status: e.target.value}))}
-                    >
-                    <option value="CURRENT">Current</option>
-                    <option value="COMPLETED">Completed</option>
-                    <option value="REPEATING">Rewatching</option>
-                    <option value="PAUSED">Paused</option>
-                    <option value="DROPPED">Dropped</option>
-                    <option value="PLANNING">Planning</option>
-                </select>
-            </label>
-            <label className='update-label'>
-                Score
-                <input 
-                    type="number" 
-                    className='label-element'
-                    value={formData.score}
-                    onChange={(e) => setFormData(prev => ({...prev, score: e.target.value}))}
-                    min={0}
-                    max={10}
-                />
-            </label>
-            <label className='update-label'>
-                Episodes
-                <input 
-                    type="number" 
-                    className='label-element'
-                    value={formData.episodes}
-                    onChange={(e) => setFormData(prev => ({...prev, episodes: e.target.value}))}
-                />
-            </label>
-            <label className='update-label'>
-                Rewatch
-                <input 
-                    type="number" 
-                    className='label-element'
-                    value={formData.repeat}
-                    onChange={(e) => setFormData(prev => ({...prev, repeat: e.target.value}))}
-                />
-            </label>
-            <label className='update-label'>
-                Start Date
-                <input 
-                    type="date" 
-                    className='label-element'
-                    value={formData.startDate}
-                    onChange={(e) => setFormData(prev => ({...prev, startDate: e.target.value}))}
-                />
-            </label>
-            <label className='mb-2 update-label'>
-                Finish Date
-                <input 
-                    type="date" 
-                    className='label-element'
-                    value={formData.finishDate}
-                    onChange={(e) => setFormData(prev => ({...prev, finishDate: e.target.value}))}
-                />
-            </label>
-            <button className='mt-3 label-element'>Submit</button>
-        </form>
-    )
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-export default Update
+  return loading ? (
+    <div className="flex w-[350px] h-[600px] bg-darkGrey justify-center items-center">
+      <Triangle height={50} width={50} color={"white"} />
+    </div>
+  ) : (
+    <form
+      onSubmit={submitHandler}
+      className="w-[350px] font-display max-h-[600px] p-3 flex flex-col bg-darkGrey"
+    >
+      <h1 className="text-white">{animeData?.media?.title?.english}</h1>
+      <img
+        src={animeData?.media?.bannerImage}
+        className="mt-2 rounded-sm"
+        alt=""
+      />
+      <label className="update-label">
+        Status
+        <select
+          className="label-element"
+          value={formData.status}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, status: e.target.value }))
+          }
+        >
+          <option value="CURRENT">Current</option>
+          <option value="COMPLETED">Completed</option>
+          <option value="REPEATING">Rewatching</option>
+          <option value="PAUSED">Paused</option>
+          <option value="DROPPED">Dropped</option>
+          <option value="PLANNING">Planning</option>
+        </select>
+      </label>
+      <label className="update-label">
+        Score
+        <input
+          type="number"
+          className="label-element"
+          value={formData.score}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, score: e.target.value }))
+          }
+          min={0}
+          max={10}
+        />
+      </label>
+      <label className="update-label">
+        Episodes
+        <input
+          type="number"
+          className="label-element"
+          value={formData.episodes}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, episodes: e.target.value }))
+          }
+        />
+      </label>
+      <label className="update-label">
+        Rewatch
+        <input
+          type="number"
+          className="label-element"
+          value={formData.repeat}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, repeat: e.target.value }))
+          }
+        />
+      </label>
+      <label className="update-label">
+        Start Date
+        <input
+          type="date"
+          className="label-element"
+          value={formData.startDate}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, startDate: e.target.value }))
+          }
+        />
+      </label>
+      <label className="mb-2 update-label">
+        Finish Date
+        <input
+          type="date"
+          className="label-element"
+          value={formData.finishDate}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, finishDate: e.target.value }))
+          }
+        />
+      </label>
+      <button className="mt-3 label-element">Submit</button>
+    </form>
+  );
+};
+
+export default Update;
